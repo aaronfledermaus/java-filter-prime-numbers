04d9c1d1cc1eec6ba536ae665500897fdf27c523
zhicheng-ning <2516085027@qq.com>
2022-04-05 07:40:08 +0000

    Update at 2022-04-05T07:40:08.424844Z

--- src/main/java/com/bytelegend/Challenge.java
+++ src/main/java/com/bytelegend/Challenge.java
@@ -1,6 +1,7 @@
 package com.bytelegend;
 
 import java.util.Arrays;
+import java.util.*;
 
 public class Challenge {
     public static void main(String[] args) {
@@ -31,7 +32,7 @@ public class Challenge {
         return res;
     }
 
-    public static boolean check(int num) {
+    public static boolean isPrime(int num) {
         if (num <= 1) return false;
         for (int i = 2; i <= (int) Math.sqrt(num); i++) {
             if (num % i == 0) return false;
