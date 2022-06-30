fb8636b58c7f4a77e17d6d581c8a6df6787c02c9
Yeely0162 <Yeely01621@163.com>
2022-06-30 03:09:49 +0000

    Update at 2022-06-30T03:09:49.852316Z

--- src/main/java/com/bytelegend/Challenge.java
+++ src/main/java/com/bytelegend/Challenge.java
@@ -1,25 +1,35 @@
 package com.bytelegend;
 
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
 
 public class Challenge {
     public static void main(String[] args) {
         System.out.println(Arrays.toString(filterPrimeNumbers(1, 10)));
         System.out.println(Arrays.toString(filterPrimeNumbers(50, 100)));
-    }
 
-    /**
-     * `filterPrimeNumbers(int start, int end)` method returns an array of all prime numbers in the
-     * given `start<=n<=end` range.
-     *
-     * <p>For example, `filterPrimeNumbers(1, 10)` returns `[2, 3, 5, 7]`.
-     *
-     * <p>A prime number is a whole number greater than 1 whose only factors are 1 and itself.
-     *
-     * <p>Tips: for a positive integer `n`, if it's not divisible by any of the integers between 2
-     * and `Math.sqrt(n)`, then `n` is a prime number.
-     */
+    }
     public static int[] filterPrimeNumbers(int start, int end) {
         return null;
+        List<Integer> result = new ArrayList<>();
+        for (int i = start; i <= end; i++) {
+            if (isPrime(i)) {
+                result.add(i);
+            }
+        }
+        return result.stream().mapToInt(Integer::intValue).toArray();
+    }
+
+    public static boolean isPrime(int num) {
+        if (num <= 1) {
+            return false;
+        }
+        for (int j = 2; j <= (int) Math.sqrt(num); j++) {
+            if (num % j == 0) {
+                return false;
+            }
+        }
+        return true;
     }
 }
