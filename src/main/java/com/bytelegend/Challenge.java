d84c73e5034653cbb083fb22b40ded1b90ff5f39
Yeely0162 <Yeely01621@163.com>
2022-06-30 03:11:26 +0000

    Update at 2022-06-30T03:11:26.196222Z

--- src/main/java/com/bytelegend/Challenge.java
+++ src/main/java/com/bytelegend/Challenge.java
@@ -10,13 +10,25 @@ public class Challenge {
         System.out.println(Arrays.toString(filterPrimeNumbers(50, 100)));
 
     }
+
+    /**
+     * `filterPrimeNumbers(int start, int end)` method returns an array of all prime numbers in the
+     * given `start<=n<=end` range.
+     *
+     * <p>For example, `filterPrimeNumbers(1, 10)` returns `[2, 3, 5, 7]`.
+     *
+     * <p>A prime number is a whole number greater than 1 whose only factors are 1 and itself.
+     *
+     * <p>Tips: for a positive integer `n`, if it's not divisible by any of the integers between 2
+     * and `Math.sqrt(n)`, then `n` is a prime number.
+     */
     public static int[] filterPrimeNumbers(int start, int end) {
-        return null;
         List<Integer> result = new ArrayList<>();
         for (int i = start; i <= end; i++) {
             if (isPrime(i)) {
                 result.add(i);
             }
+
         }
         return result.stream().mapToInt(Integer::intValue).toArray();
     }
