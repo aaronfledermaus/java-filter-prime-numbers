845f6481b31f657e73fb34e67c7829856743cda7
chushi0 <wangyongchao49@sina.com>
2022-07-07 00:29:53 +0000

    Update at 2022-07-07T00:29:53.541315Z

--- src/main/java/com/bytelegend/Challenge.java
+++ src/main/java/com/bytelegend/Challenge.java
@@ -27,7 +27,7 @@ public class Challenge {
                 continue;
             }
             boolean isPrime = true;
-            for (int j = 2; j < Math.sqrt(i); j++) {
+            for (int j = 2; j <= Math.sqrt(i); j++) {
                 if (i % j == 0) {
                     isPrime = false;
                     break;
