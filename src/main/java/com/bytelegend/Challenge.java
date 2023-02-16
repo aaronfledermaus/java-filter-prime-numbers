fce75562c92eeaa07d04b7bf83e59fb3875ba802
AaronFledermaus <78683536+aaronfledermaus@users.noreply.github.com>
2023-02-16 18:58:34 +0800

    Update Challenge.java

--- src/main/java/com/bytelegend/Challenge.java
+++ src/main/java/com/bytelegend/Challenge.java
@@ -1,6 +1,7 @@
 package com.bytelegend;
 
 import java.util.Arrays;
+import java.util.List;
 import java.util.ArrayList;
 
 public class Challenge {
@@ -20,19 +21,25 @@ public class Challenge {
      * <p>Tips: for a positive integer `n`, if it's not divisible by any of the integers between 2
      * and `Math.sqrt(n)`, then `n` is a prime number.
      */
-    public static int[] filterPrimeNumbers(int start, int end) { // if prime -> new int[] -> int[++] 
+    public static int[] filterPrimeNumbers(int start, int end) {
         ArrayList<Integer> primeNumbers = new ArrayList<Integer>();
         for(int i=start; i<=end; i++)
             if(prime(i)) // if i prime
                 primeNumbers.add(i);
-        int[] result = new int[primeNumbers.size()];
-        for(int i=0; i<primeNumbers.size(); i++)
-            result[i] = primeNumbers.get(i);
-        return result;
+        if(primeNumbers==null||start>end)
+            return new int[]{};
+        else{
+            int[] result = new int[primeNumbers.size()];
+            for(int i=0; i<primeNumbers.size(); i++)
+                result[i] = primeNumbers.get(i);
+            return result;
+        }
     }
-    public static boolean prime(int a) { //Illegal modifier for parameter prime; only final is permitted    //Cannot make a static reference to the non-static method prime(int) from the type Challenge
+    public static boolean prime(int a) {
         if(a<=1)
             return false;
+        if(a==2)
+            return true;
         for(int b=2; b<Math.sqrt(a)+1; b++)
             if(a%b==0)
                 return false;
